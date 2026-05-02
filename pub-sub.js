699392f36ad8b86180428e5d6a25b00ba766e601
hiralazure <hiralazure2025@gmail.com>
2026-05-02 21:38:11 +0100

    adding pipeline and transction

--- pub-sub.js
+++ pub-sub.js
@@ -29,6 +29,56 @@ async function testAdditionalFeatures() {
     await new Promise((resolve) => setTimeout(resolve, 3000));
     await subscriber.unsubscribe("dummy-channel");
     await subscriber.quit(); //close the subscriber
+
+    //pipelining & transition
+    //sending multiple commands to redis server in batch
+    //banking appplication money from one account to another account, one operatin to fail and another to succeed
+    // const multi = client.multi();
+    // multi.set("key-transcation1", "value1");
+    // multi.set("key-transcation2", "value2");
+    // multi.get("key-transcation1");
+    // multi.get("key-transcation2");
+    // const results = await multi.exec();
+    // console.log(results);
+
+    // const pipeline = client.multi();
+    // multi.set("key-pipeline1", "value1");
+    // multi.set("key-pipeline2", "value2");
+    // multi.get("key-pipeline1");
+    // multi.get("key-pipeline2");
+    // const pipelineresults = await multi.exec();
+    // console.log(pipelineresults);
+
+    // //batch data operation
+    // const pipelineOne = client.multi();
+    // for (i = 0; i < 1000; i++) {
+    //   pipeline.set(`user:${i}:action`, `Action ${1}`);
+    // }
+    // await pipelineOne.exec();
+
+    // const dummyExample = await client.multi();
+    // multi.decrBy("account:1234:balance", 100);
+    // multi.incrBy("account:0000:balance", 100);
+    // const finalresults = await multi.exec();
+
+    // const cartEaxmnple = client.multi();
+    // multi.hIncrBy("cart:1234", "item_count", 1);
+    // multi.hIncrBy("cart:1234", "item_price", 10);
+    // await multi.exec();
+    console.log("performance test");
+    console.time("without pipeline");
+    for (let i = 0; i < 1000; i++) {
+      await client.set(`user${i}`, `user_value${1}`);
+    }
+    console.timeEnd("without pipeline");
+
+    console.time("with pipeline");
+    const bigPipelint = client.multi();
+    for (let i = 0; i < 1000; i++) {
+      await bigPipelint.set(`user_pipelinet${i}`, `user_pipeline_value${1}`);
+    }
+    await bigPipelint.exec();
+    console.timeEnd("with pipeline");
   } catch (e) {
     console.error(e);
   } finally {
